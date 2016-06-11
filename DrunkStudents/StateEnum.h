48cb105eea28e3fc9d2116bf0a027e0bb450cefe
Yamadads <spawlak@yahoo.com>
2016-06-11 15:18:10 +0200

    states

---
+++ DrunkStudents/StateEnum.h
@@ -0,0 +1,16 @@
+//
+// Created by debian on 11.06.16.
+//
+
+#ifndef DRUNKSTUDENTS_STATEENUM_H
+#define DRUNKSTUDENTS_STATEENUM_H
+
+#endif //DRUNKSTUDENTS_STATEENUM_H
+
+#pragma once
+
+#include <string>
+
+enum stateEnum { NOT_WANT_DRINK, WANT_DRINK, WANT_ARBITER, DRINK };
+
+const std::string stateName[] = {"I do not want to drink", "I want to drink", "I want the arbiter", "I'm drinking"};
